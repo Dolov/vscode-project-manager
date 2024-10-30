544d8eec24ddf76d694a5a1a61af46e1a42b868b
Dolov <songyan_shi@163.com>
2024-10-30 22:34:46 +0800

    format

--- src/TreeDataProvider.ts
+++ src/TreeDataProvider.ts
@@ -1,97 +1,102 @@
-import * as vscode from 'vscode';
-import { ProjectItemProps, getBranchName, store } from './utils'
+import * as vscode from "vscode";
+import { ProjectItemProps, getBranchName, store } from "./utils";
 
 export class CurrentProvider implements vscode.TreeDataProvider<Project> {
-
-	private _onDidChangeTreeData: vscode.EventEmitter<Project | undefined | null | void> = new vscode.EventEmitter<Project | undefined | null | void>();
-  readonly onDidChangeTreeData: vscode.Event<Project | undefined | null | void> = this._onDidChangeTreeData.event;
-
-  async refresh() {
-		this._onDidChangeTreeData.fire();
-  }
-
-	constructor(private workspaceRoot: string | undefined) {
-	}
-
-	get count() {
-		return store.current.length
-	}
-
-	getTreeItem(element: Project): vscode.TreeItem {
-		return element;
-	}
-
-	getChildren(element?: Project): Thenable<Project[]> {
-    const favorites: ProjectItemProps[] = store.favorite
-		return new Promise(async resolve => {
-			const items = store.current.map(item => {
-				return new Project(item, favorites)
-			})
-			resolve(items)
-		})
-	}
-}
-
-
-export class FavoriteProvider implements vscode.TreeDataProvider<Project> {
-
-	private _onDidChangeTreeData: vscode.EventEmitter<Project | undefined | null | void> = new vscode.EventEmitter<Project | undefined | null | void>();
-  readonly onDidChangeTreeData: vscode.Event<Project | undefined | null | void> = this._onDidChangeTreeData.event;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    Project | undefined | null | void
+  > = new vscode.EventEmitter<Project | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    Project | undefined | null | void
+  > = this._onDidChangeTreeData.event;
 
   async refresh() {
     this._onDidChangeTreeData.fire();
   }
 
-	constructor(private workspaceRoot: string | undefined) {
-	}
+  constructor(private workspaceRoot: string | undefined) {}
 
-	get count() {
-		return store.favorite.length
-	}
+  get count() {
+    return store.current.length;
+  }
 
-	getTreeItem(element: Project): vscode.TreeItem {
-		return element;
-	}
+  getTreeItem(element: Project): vscode.TreeItem {
+    return element;
+  }
 
-	getChildren(element?: Project): Thenable<Project[]> {
-    return new Promise(async resolve => {
-			const projects: ProjectItemProps[] = store.favorite
-			const items = projects.map(itemc => {
-				const { path } = itemc
-				const branchName = getBranchName(path)
-				return new Project({
-					...itemc,
-					branchName,
-				}, projects)
-			})
-			resolve(items)
-		})
-	}
+  getChildren(element?: Project): Thenable<Project[]> {
+    const favorites: ProjectItemProps[] = store.favorite;
+    return new Promise(async (resolve) => {
+      const items = store.current.map((item) => {
+        return new Project(item, favorites);
+      });
+      resolve(items);
+    });
+  }
+}
+
+export class FavoriteProvider implements vscode.TreeDataProvider<Project> {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    Project | undefined | null | void
+  > = new vscode.EventEmitter<Project | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    Project | undefined | null | void
+  > = this._onDidChangeTreeData.event;
+
+  async refresh() {
+    this._onDidChangeTreeData.fire();
+  }
+
+  constructor(private workspaceRoot: string | undefined) {}
+
+  get count() {
+    return store.favorite.length;
+  }
+
+  getTreeItem(element: Project): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: Project): Thenable<Project[]> {
+    return new Promise(async (resolve) => {
+      const projects: ProjectItemProps[] = store.favorite;
+      const items = projects.map((itemc) => {
+        const { path } = itemc;
+        const branchName = getBranchName(path);
+        return new Project(
+          {
+            ...itemc,
+            branchName,
+          },
+          projects
+        );
+      });
+      resolve(items);
+    });
+  }
 }
 
 export class Project extends vscode.TreeItem {
+  contextValue = "favorite";
 
-	contextValue = "favorite"
+  constructor(
+    public readonly item: ProjectItemProps,
+    public readonly config: ProjectItemProps[]
+  ) {
+    const { path, name, branchName } = item;
+    super(name);
+    this.iconPath = new vscode.ThemeIcon("folder");
+    this.tooltip = `${name}`;
 
-	constructor(
-		public readonly item: ProjectItemProps,
-		public readonly config: ProjectItemProps[]
-	) {
-		const { path, name, branchName } = item
-		super(name);
-		this.iconPath = new vscode.ThemeIcon('folder');
-		this.tooltip = `${name}`;
+    this.description = branchName;
+    this.command = {
+      title: "Open Project",
+      command: "project-manager.openProject",
+      arguments: [item],
+    };
 
-		this.description = branchName;
-		this.command = {
-			title: 'Open Project',
-			command: 'project-manager.openProject',
-			arguments: [item]
-		}
-
-		const target = config.find(item => item.path === path)
-		if (target) {
-			this.contextValue = "favorited"
-		}
-	}
+    const target = config.find((item) => item.path === path);
+    if (target) {
+      this.contextValue = "favorited";
+    }
+  }
 }
